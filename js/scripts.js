14f540717ccb52dbc2bee9ab49ed213171c9f267
Amiel Paolo C. Liboro <amiel_liboro@yahoo.com.ph>
2019-01-29 15:39:58 +0800

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+});
+$("#loginButton").click(function(){
+    $('#loginModal').modal('show'); 
+});
+$("#loginClose").click(function(){
+    $('#loginModal').modal('hide'); 
+});
+$("#loginCancel").click(function(){
+    $('#loginModal').modal('hide'); 
+});
+$("#reserveButton").click(function(){
+    $('#reserveModal').modal('show'); 
+});
+$("#reserveClose").click(function(){
+    $('#reserveModal').modal('hide'); 
+});
+$("#reserveCancel").click(function(){
+    $('#reserveModal').modal('hide'); 
+});
